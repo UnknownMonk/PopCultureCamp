2b96acbe76c8c975c281ff8efbbb9554bf5115d9
UnknowMonk <smodcartoon@gmail.com>
2018-02-17 17:35:13 -0500

    uptated

--- app.js
+++ app.js
@@ -17,29 +17,40 @@ var commentRoutes = require("./routes/comments"),
     indexRoutes = require("./routes/index")
 
 
-mongoose.connect(mongodb: //UnknownMonk:&roundcl0ud@ds239988.mlab.com:39988/yelpcamp);
+mongoose.connect("mongodb: //UnknownMonk:&roundcl0ud@ds239988.mlab.com:39988/yelpcamp");
 
 
-        app.use(bodyParser.urlencoded({ extended: true })); app.set("view engine", "ejs"); app.use(express.static(__dirname + "/public")); app.use(methodOverride("_method")); app.use(flash());
-        // seedDB(); //seed the database
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+// seedDB(); //seed the database
 
-        // PASSPORT CONFIGURATION
-        app.use(require("express-session")({
-            secret: "Once again Rusty wins cutest dog!",
-            resave: false,
-            saveUninitialized: false
-        })); app.use(passport.initialize()); app.use(passport.session()); passport.use(new LocalStrategy(User.authenticate())); passport.serializeUser(User.serializeUser()); passport.deserializeUser(User.deserializeUser());
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-        app.use(function(req, res, next) {
-            res.locals.currentUser = req.user;
-            res.locals.error = req.flash("error");
-            res.locals.success = req.flash("success");
-            next();
-        });
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
 
-        app.use("/", indexRoutes); app.use("/campgrounds", campgroundRoutes); app.use("/campgrounds/:id/comments", commentRoutes);
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-        app.listen(process.env.PORT, process.env.IP, function() {
-            console.log("The YelpCamp Server Has Started!");
-        });
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("The YelpCamp Server Has Started!");
+});
